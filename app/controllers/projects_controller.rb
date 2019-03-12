9585033626cd5079a1569819f085f79d86e41799
Antoine <Dember224@gmail.com>
2019-03-12 05:14:21 -0400

    Controller logic and view for handleing new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,19 @@
 class ProjectsController < ApplicationController
   def new
-
+    @current_team = Team.find_by_id(session[:team_id])
+    @team_name = @current_team.team_name
+    @team_id = @current_team.team_id
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
   end
-  
+
+  private
+
+  def project_params
+    params.require(:project).permit(:team, :project_name, :deadline, :project_description)
+  end
+
 end
