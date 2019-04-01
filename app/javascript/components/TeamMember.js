c2f5ac7c8f0cc829259c12cb0dac737e451c2254
Antoine <Dember224@gmail.com>
2019-04-01 14:29:12 -0400

    Included new material and footers in teamMembers

--- app/javascript/components/TeamMember.js
+++ app/javascript/components/TeamMember.js
@@ -1,6 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {Assignments} from "./Assignments.js"
+import {Materials} from "./Materials.js"
+
 class TeamMember extends React.Component {
   render () {
     return (
@@ -8,7 +10,10 @@ class TeamMember extends React.Component {
         <div className="team_members">
           <p>{this.props.first_name} {this.props.last_name}</p>
           <Assignments description ={this.props.description} deadline ={this.props.deadline} className="team_members" />
+          <p>Responsible for the following materials:</p>
+          <Materials material={this.props.material} />
         </div>
+
       </React.Fragment>
     );
   }
