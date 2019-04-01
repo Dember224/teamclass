157ee8027432038d8d23d20f50153e929022dc4b
Antoine <Dember224@gmail.com>
2019-04-01 00:40:31 -0400

    correcting logout and signup routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
   get '/projects' => 'projects#new'
   post 'projects' => 'projects#create'
   get 'our_project' => 'projects#index'
