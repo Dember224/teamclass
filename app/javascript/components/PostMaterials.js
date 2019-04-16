96afb5b80e5033aba3b9f00f08e7aec6ef334953
Antoine <Dember224@gmail.com>
2019-04-16 02:43:05 -0400

    making materials form display toggleable within the projects index view

--- app/javascript/components/PostMaterials.js
+++ app/javascript/components/PostMaterials.js
@@ -1,12 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {InputMaterials} from "./InputMaterials.js"
+import {TogglePostMaterials} from "./TogglePostMaterials.js"
+
 
 export class PostMaterials extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "hide_members"
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+
+  toggleDisplay(){
+    if (this.state.display === "hide_members") {
+      this.setState({
+        display: "materials_form"
+      })
+    } else {
+      this.setState({
+        display: "hide_members"
+      })
+    }
+
+  }
   render () {
     return (
       <React.Fragment>
-        <form className="new_material" id="new_material" action="/materials" accept-charset="UTF-8" data-remote="true" method="post">
+        <TogglePostMaterials onClick={this.toggleDisplay}/ >
+        <div className={this.state.display}>
+          <form className="new_material" id="new_material" action="/materials" accept-charset="UTF-8" data-remote="true" method="post">
           <input name="utf8" type="hidden" value="&#x2713;" />
           <input value={this.props.project_id} type="hidden" name="material[project_id]" id="material_project_id" />
           <p>Name of the material</p>
@@ -21,6 +46,7 @@ export class PostMaterials extends React.Component {
 
           <input type="submit" name="commit" value="Add this material" data-disable-with="Add this material" />
         </form>
+        </div>
 
       </React.Fragment>
     );
---
+++ app/javascript/components/TogglePostMaterials.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+export class TogglePostMaterials extends React.Component {
+  handleChange(e){
+    const showMaterial = e.target.value
+    this.props.onClick(showMaterial)
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <div className="project_links" onClick={this.props.onClick}>List the materials you need to complete this project</div>
+      </React.Fragment>
+    );
+  }
+}
